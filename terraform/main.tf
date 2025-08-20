fbaa77723e7ced5a1bf73d998a177a5ccd325080
Vinod-M-git <vinodm@devtools.in>
2025-08-20 10:40:55 +0530

    done

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket         = "s3state0989"
+    key            = "ecr-sync-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "ecr_demo" {
+  source      = "./modules/ecr"
+  name        = "demo"
+  environment = "dev"
+}
---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "demo_repo_url" {
+  value = module.ecr_demo.repository_url
+}
+ 
