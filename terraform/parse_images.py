5e0be615a00c4794800245ffe13e84c7acb30863
Vinod-M-git <vinodm@devtools.in>
2025-08-20 14:41:24 +0530

    done with retaging feature

--- terraform/parse_images.py
+++ terraform/parse_images.py
@@ -7,7 +7,7 @@ images_file = os.path.join(repo_root, "images.txt")
 with open(images_file) as f:
     lines = [l.strip() for l in f if l.strip()]
 
-# Support multiple images
+
 images = {}
 for idx, line in enumerate(lines):
     name, tag = line.split(":")
